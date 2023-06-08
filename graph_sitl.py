19b087e1bf7e2fd612f93012b428931dba3bcdea
Clyde McQueen <clyde@mcqueen.net>
2023-06-07 17:46:25 -0700

    x-axis is time, not index

--- graph_sitl.py
+++ graph_sitl.py
@@ -14,28 +14,37 @@ import pandas as pd
 def graph_sitl(csv_path, pdf_path):
     df = pd.read_csv(csv_path)
 
+    # Rebase timestamp to start at 0
+    start = df['timestamp'][0]
+    df['timestamp'] = df['timestamp'].add(-start)
+
+    # Create 1 figure with 3 subplots
     fig, (ax_alt, ax_rf, ax_crt) = plt.subplots(3)
 
-    # From the CTUN log entry in the ArduSub dataflash log
-    df['DAlt'].plot(ax=ax_alt)
-    df['Alt'].plot(ax=ax_alt)
-    df['BAlt'].plot(ax=ax_alt)
+    # Add CTUN fields
+    ax_alt.plot('timestamp', 'DAlt', data=df, label='CTUN target z')
+    ax_alt.plot('timestamp', 'Alt', data=df, label='CTUN EKF z')
+    ax_alt.plot('timestamp', 'BAlt', data=df, label='CTUN barometer z')
 
-    df['DSAlt'].plot(ax=ax_rf)
-    df['SAlt'].plot(ax=ax_rf)
+    ax_rf.plot('timestamp', 'DSAlt', data=df, label='CTUN target rangefinder')
+    ax_rf.plot('timestamp', 'SAlt', data=df, label='CTUN rangefinder')
 
-    df['DCRt'].plot(ax=ax_crt)
-    df['CRt'].plot(ax=ax_crt)
+    ax_crt.plot('timestamp', 'DCRt', data=df, label='CTUN target climb rate')
+    ax_crt.plot('timestamp', 'CRt', data=df, label='CTUN climb rate')
 
-    # From sub.py
-    df['sub_z'].plot(ax=ax_alt)
-    df['terrain_z'].plot(ax=ax_alt)
+    # Add stamped_terrain.csv fields
+    ax_alt.plot('timestamp', 'sub_z', data=df, label='Injection-time z')
+    ax_alt.plot('timestamp', 'terrain_z', data=df, label='Terrain z')
 
-    df['rf'].plot(ax=ax_rf)
+    ax_rf.plot('timestamp', 'rf', data=df, label='Injected rangefinder')
 
-    ax_alt.legend(labels=['CTUN target z', 'CTUN EKF z', 'CTUN barometer z', 'Injection-time z', 'Terrain z'])
-    ax_rf.legend(labels=['CTUN target rangefinder', 'CTUN rangefinder', 'Injected rangefinder'])
-    ax_crt.legend(labels=['CTUN target climb rate', 'CTUN climb rate'])
+    ax_alt.legend()
+    ax_rf.legend()
+    ax_crt.legend()
+
+    ax_alt.grid(axis='x')
+    ax_rf.grid(axis='x')
+    ax_crt.grid(axis='x')
 
     plt.savefig(pdf_path)
 
